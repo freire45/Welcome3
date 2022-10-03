aa23eb2db283ab478e198f7044448bd6b8bac1b6
Erick Freire <erilefr@gmail.com>
2022-10-02 21:36:58 -0300

    version1
    version1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Welcome3
  Welcome To Java Programming - Version 3
+
+## Image
+![Currículo no Computador](welcom_3.png)
---
+++ Welcome3/src/br/com/erickfreire/welcome3/Welcome3.java
@@ -0,0 +1,16 @@
+package br.com.erickfreire.welcome3;
+
+/**
+ * Welcome To Java Programming - Version 3
+ * @author Erick Freire
+ * @version 1 - Created in 10-02-2022
+ */
+
+public class Welcome3 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome\nTo\nJava\nProgramming!");
+
+	}
+
+}
